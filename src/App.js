b89984ea5835f30a05c4d165dc70b580922532ad
Abhay <26abhaykumarsingh@gmail.com>
2024-02-11 15:16:54 +0530

    ADD : HomePage, Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
+import HomePage from "./HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage></HomePage>,
+  },
+]);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
